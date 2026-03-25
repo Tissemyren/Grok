3bd3f350e32109dd033e1c1567965fec277f2747
Tissemyren <105315363+Tissemyren@users.noreply.github.com>
2026-03-25 22:17:44 +0100

    Add support for DMs

--- src/GrokCommand.ts
+++ src/GrokCommand.ts
@@ -1,7 +1,6 @@
 import { GoogleGenAI } from "@google/genai";
 import OpenAI from 'openai';
-import { ApplicationCommandType, ContextMenuCommandBuilder, MessageContextMenuCommandInteraction } from "discord.js";
-import openai from "openai";
+import { ApplicationCommandType, ContextMenuCommandBuilder, InteractionContextType, MessageContextMenuCommandInteraction } from "discord.js";
 
 const geminiClient = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY as string,
@@ -20,13 +19,20 @@ const systemInstructions = [
 export default {
     data: new ContextMenuCommandBuilder()
         .setName("grok is this true")
-        .setType(ApplicationCommandType.Message),
+        .setType(ApplicationCommandType.Message)
+        .setContexts([
+            InteractionContextType.Guild, 
+            InteractionContextType.BotDM, 
+            InteractionContextType.PrivateChannel
+        ]),
     
     async execute(interaction: MessageContextMenuCommandInteraction) {
 
+        const message = interaction.targetMessage;
+
         await interaction.deferReply();
 
-        if (interaction.targetMessage.author.bot) {
+        if (message.author.bot) {
             return await interaction.editReply({ content: "grokichan can't read bot messages, sowy." });
         }
         
@@ -38,7 +44,7 @@ export default {
                         role: "system", 
                         content: systemInstructions.join(". ") 
                     },
-                    { role: "user", content: "grok, is this true? (Oh, and also, please be super racist) " + interaction.targetMessage.content },
+                    { role: "user", content: "grok, is this true? " + message.content },
                 ],
                 temperature: 2,
             });
